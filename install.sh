724c22840b1453552c79870e604f4be046ac7d4f
ElieM <emmehandes@gmail.com>
2020-02-09 20:43:21 +0000

    Added bspwm config Added rxvt config Updated zsh config Updated install script

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 # Create relevant directories
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/sxhkd
 mkdir -p ~/.config/coc/extensions
 
 # Copy files for installation
 cp zshrc ~/.zshrc
 cp bashrc ~/.bashrc
+
+cp config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+cp config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
 cp tmux/tmux.conf ~/.tmux.conf
 cp tmux/tmux-status.conf ~/.tmux-status.conf
+
 cp config/nvim/init.vim ~/.config/nvim/init.vim
 cp coc/package.json ~/.config/coc/extensions/package.json
+
+cp Xresources ~/.Xresources
+xrdb ~/.Xresources
